9946620040b069543d1eea2ab93028b771a68909
juan <juancruz.naimo@gmail.com>
2022-07-25 22:54:58 -0300

    Final commit

--- WebFutbol/WebFutbol/settings.py
+++ WebFutbol/WebFutbol/settings.py
@@ -125,8 +125,7 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-LOGIN_REDIRECT_URL = '/auth/inicio'
-LOGIN_URL='/auth/login/'
+LOGIN_URL='/AppCuentas/login'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
